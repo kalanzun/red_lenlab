79151280419823597c41fd87e22e92fa942ced94
Christoph Simon <christoph.simon@kit.edu>
2017-10-07 12:48:22 +0200

    Enable buffered uartstdio

--- tm4c1230h6pm_startup_ccs.c
+++ tm4c1230h6pm_startup_ccs.c
@@ -54,7 +54,7 @@ extern uint32_t __STACK_TOP;
 // External declarations for the interrupt handlers used by the application.
 //
 //*****************************************************************************
-// To be added by user
+extern void UARTStdioIntHandler(void);
 
 //*****************************************************************************
 //
@@ -88,7 +88,7 @@ void (* const g_pfnVectors[])(void) =
     IntDefaultHandler,                      // GPIO Port C
     IntDefaultHandler,                      // GPIO Port D
     IntDefaultHandler,                      // GPIO Port E
-    IntDefaultHandler,                      // UART0 Rx and Tx
+    UARTStdioIntHandler,                    // UART0 Rx and Tx
     IntDefaultHandler,                      // UART1 Rx and Tx
     IntDefaultHandler,                      // SSI0 Rx and Tx
     IntDefaultHandler,                      // I2C0 Master and Slave
