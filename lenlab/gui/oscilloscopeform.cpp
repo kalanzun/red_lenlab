9152c4d52826dd25e0df329ec0296bbf9eeba753
Christoph Simon <post@christoph-simon.eu>
2018-01-18 22:08:52 +0100

    Oscilloscope channel colors blue and green

--- lenlab/gui/oscilloscopeform.cpp
+++ lenlab/gui/oscilloscopeform.cpp
@@ -67,9 +67,9 @@ OscilloscopeForm::setModel(model::Lenlab *lenlab)
     this->lenlab = lenlab;
     this->oscilloscope = lenlab->oscilloscope;
 
-    curves[0] = newCurve(QColor("#fce94f"), true); // butter 0
+    curves[0] = newCurve(QColor("#729fcf"), true); // sky blue 0
     curves[1] = newCurve(QColor("#8ae234"), true); // green 0
-    //curves[2] = newCurve(QColor("#729fcf"), false); // sky blue 0
+    //curves[0] = newCurve(QColor("#fce94f"), true); // butter 0
     //curves[3] = newCurve(QColor("#ef2929"), false); // scarlet red 0
 
     ui->samplerateBox->insertItems(0, oscilloscope->samplerateIndex.labels);
