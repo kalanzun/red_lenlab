7a8b772b80934f0854e7dacc131b9d6c1da251f4
Christoph Simon <christoph.simon@kit.edu>
2019-04-08 14:17:57 +0200

    oscilloscope tests

--- firmware/tests/test_oscilloscope.c
+++ firmware/tests/test_oscilloscope.c
@@ -8,74 +8,97 @@
 
 #include "test_oscilloscope.h"
 
-#include "oscilloscope.h"
 #include "memory.h"
 #include "microtest.h"
-#include "int_timer.h"
+#include "oscilloscope.h"
 
-/*
+
 void
 test_oscilloscope_lock(void)
 {
     test();
 
-    assertFalse(oscilloscope.lock);
-    assertOK(OscilloscopeStart(&oscilloscope, 1000));
-    assertTrue(oscilloscope.lock);
-    assertTrue(adc_group.lock);
-    assertOK(OscilloscopeStop(&oscilloscope));
-    assertFalse(oscilloscope.lock);
-    assertFalse(adc_group.lock);
+    assert(oscilloscope.lock == false);
+    assert(OscilloscopeStart(&oscilloscope, 1) == OK);
+    assert(oscilloscope.lock == true);
+    assert(adc_group.lock == true);
+    assert(memory.lock == true);
+
+    // wait for the measurement to finish
+    while (!OscSeqGroupReady(&oscilloscope.seq_group)) {};
+
+    assert(OscilloscopeStop(&oscilloscope) == OK);
+    assert(oscilloscope.lock == false);
+    assert(adc_group.lock == false);
+    assert(memory.lock == false);
 
     ok();
 }
-*/
-/*
+
+
 void
 test_oscilloscope_double_start(void)
 {
     test();
 
-    assertOK(OscilloscopeStart(&oscilloscope, 1000));
-    assertError(OscilloscopeStart(&oscilloscope, 1000), LOCK_ERROR);
-    assertOK(OscilloscopeStop(&oscilloscope));
+    assert(OscilloscopeStart(&oscilloscope, 1) == OK);
+    assert(OscilloscopeStart(&oscilloscope, 1) == LOCK_ERROR);
+
+    // wait for the measurement to finish
+    while (!OscSeqGroupReady(&oscilloscope.seq_group)) {};
+
+    assert(OscilloscopeStop(&oscilloscope) == OK);
 
     ok();
 }
-*/
-/*
+
 void
 test_oscilloscope_double_stop(void)
 {
     test();
 
-    assertOK(OscilloscopeStart(&oscilloscope, 1000));
-    assertOK(OscilloscopeStop(&oscilloscope));
-    assertError(OscilloscopeStop(&oscilloscope), LOCK_ERROR);
+    assert(OscilloscopeStart(&oscilloscope, 1) == OK);
+
+    // wait for the measurement to finish
+    while (!OscSeqGroupReady(&oscilloscope.seq_group)) {};
+
+    assert(OscilloscopeStop(&oscilloscope) == OK);
+    assert(OscilloscopeStop(&oscilloscope) == LOCK_ERROR);
 
     ok();
 }
-*/
-/*
+
+
 void
 test_oscilloscope_state_error(void)
 {
-    tError err = OK;
     test();
 
     adc_group.lock = true;
-    err = OscilloscopeStart(&oscilloscope, 1000);
-    adc_group.lock = false; // reset lock even if the assertion fails
+    assert(OscilloscopeStart(&oscilloscope, 1000) == STATE_ERROR);
+    adc_group.lock = false;
 
-    assertError(err, STATE_ERROR);
     ok();
 }
-*/
+
+
+void
+test_oscilloscope_memory_error(void)
+{
+    test();
+
+    memory.lock = true;
+    assert(OscilloscopeStart(&oscilloscope, 1000) == MEMORY_ERROR);
+    memory.lock = false;
+
+    ok();
+}
+
 
 void
 test_oscilloscope_measurement()
 {
-    int i, j;
+    unsigned int i, j;
     tRingIter iter;
 
     tRing *self;
@@ -102,17 +125,17 @@ test_oscilloscope_measurement()
             // head
             for (j = 0; j < 6; j++) {
                 if (page->buffer[j] != 0xFFFFFFFF)
-                    fail("head (%i, %i, %i)", i, iter.read, j);
+                    fail("head (adc[%i], page[%i], buffer[%i])", i, iter.read, j);
             }
             // look for alignment error
             // if the alignment is off, uDMA starts at a later address
             if (*(uint8_t *) (page->buffer + 6) == 0xFF)
-                fail("alignment (%i, %i)", i, iter.read);
+                fail("alignment (adc[%i], page[%i])", i, iter.read);
             // measurement values
             for (j = 0; j < OSCILLOSCOPE_SAMPLES; j++) {
                 value = (uint16_t *) (page->buffer + 6) + j;
                 if (*value == 0xFFFF)
-                    fail("value (%i, %i, %i)", i, iter.read, j);
+                    fail("value (adc[%i], page[%i], buffer[%i])", i, iter.read, j);
             }
         }
     }
@@ -124,5 +147,10 @@ test_oscilloscope_measurement()
 void
 test_oscilloscope()
 {
+    test_oscilloscope_lock();
+    test_oscilloscope_double_start();
+    test_oscilloscope_double_stop();
+    test_oscilloscope_state_error();
+    test_oscilloscope_memory_error();
     test_oscilloscope_measurement();
 }
