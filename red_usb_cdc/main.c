55d2ef088623c706db1da26618c7597b40460ae2
Christoph Simon <christoph.simon@kit.edu>
2021-11-23 19:10:21 +0100

    vendorize driverlib and usblib

--- red_usb_cdc/main.c
+++ red_usb_cdc/main.c
@@ -1,9 +1,105 @@
 
+#include <stdbool.h>
+#include <stdint.h>
 
-/**
- * main.c
- */
-int main(void)
+#include "com_device.h"
+
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/hw_gpio.h"
+#include "inc/hw_sysctl.h"
+#include "driverlib/debug.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/rom.h"
+#include "driverlib/rom_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/systick.h"
+#include "driverlib/timer.h"
+
+//*****************************************************************************
+//
+// The system tick rate expressed both as ticks per second and a millisecond
+// period.
+//
+//*****************************************************************************
+#define SYSTICKS_PER_SECOND 100
+#define SYSTICK_PERIOD_MS (1000 / SYSTICKS_PER_SECOND)
+
+
+//*****************************************************************************
+//
+// This is the main application entry function.
+//
+//*****************************************************************************
+int
+main(void)
 {
-	return 0;
+    //
+    // Enable lazy stacking for interrupt handlers.  This allows floating-point
+    // instructions to be used within interrupt handlers, but at the expense of
+    // extra stack usage.
+    //
+    //MAP_FPULazyStackingEnable();
+
+    //
+    // System Clock 80 MHz
+    //
+    MAP_SysCtlClockSet(SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
+
+    //
+    // Configure the required pins for USB operation.
+    //
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
+    MAP_GPIOPinTypeUSBAnalog(GPIO_PORTD_BASE, GPIO_PIN_5 | GPIO_PIN_4);
+
+    //
+    // Enable the GPIO port that is used for the on-board LED.
+    //
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+
+    //
+    // Enable the GPIO pins for the LED (PF2 & PF3).
+    //
+    MAP_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3|GPIO_PIN_2);
+
+    //
+    // Enable the system tick.
+    //
+    MAP_SysTickPeriodSet(MAP_SysCtlClockGet() / SYSTICKS_PER_SECOND);
+    MAP_SysTickIntEnable();
+    MAP_SysTickEnable();
+
+    //
+    // Initialize USB
+    //
+    COMDeviceInit();
+
+    //
+    // Main application loop.
+    //
+    while(1)
+    {
+        //
+        // Turn on the Green LED.
+        //
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, GPIO_PIN_3);
+
+        //
+        // Delay for a bit.
+        //
+        SysCtlDelay(MAP_SysCtlClockGet() / 3 / 20);
+
+        //
+        // Turn off the Green LED.
+        //
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 0);
+
+        //
+        // Delay for a bit.
+        //
+        SysCtlDelay(MAP_SysCtlClockGet() / 3 / 20);
+    }
 }
